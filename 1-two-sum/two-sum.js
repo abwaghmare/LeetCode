d51efd0e9ca457e3e15ef37c4063420529c24126
Abhay Waghmare <138000854+abwaghmare@users.noreply.github.com>
2025-05-08 19:58:41 +0530

    Time: 35 ms (24.97%) | Memory: 56 MB (23.40%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,21 +6,14 @@
 var twoSum = function(nums, target) {
     
     let arr=[];
-    let i=0;
-    let j=i+1;
 
-    for(let k=0; k<nums.length; k++){
-        if(nums[i]+nums[j] === target){
-            arr[0]=i;
-            arr[1]=j;
-        }
-        else if(j>=nums.length-1){
-            i++;
-            j=i+1;
-            k=i;
-        }
-        else{
-            j++;
+    for(let i=0; i<nums.length; i++){
+        let c = target-nums[i];
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[j] === c){
+                arr[0]=i;
+                arr[1]=j;
+            }
         }
     }
 
